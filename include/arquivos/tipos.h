c295827fa4c8973af9ef86258706add5df6c8b75
João Gomes <joaop.gomes@usp.br>
2023-11-17 20:28:14 -0300

    adiciona arquivos/tipos.h

---
+++ include/arquivos/tipos.h
@@ -0,0 +1,31 @@
+typedef struct {
+    char status;
+    int proxRRN;
+    int nroTecnologias;
+    int nroParesTecnologias;
+} Cabecalho;
+
+typedef struct {
+    int tamanho;
+    char *string;
+} StringVariavel;
+
+typedef struct {
+    char removido;
+    int grupo;
+    int popularidade;
+    int peso;
+    StringVariavel tecnologiaOrigem;
+    StringVariavel tecnologiaDestino;
+} Registro;
+
+
+/// @brief Enum for return features' return types
+typedef enum{
+    /// @brief Sucess!
+    sucesso = 0,
+    /// @brief Falha no processamento do arquivo.
+    falha_processamento = 1,
+    /// @brief Registro inexistente.
+    registro_inexistente = 2,
+} StatusDeRetorno;
