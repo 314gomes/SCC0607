af7cfff2f87b25202dc37244988da389b9287440
João Gomes <joaop.gomes@usp.br>
2023-11-19 14:02:03 -0300

    Adiciona básicos de árvore b

---
+++ include/arvoreb/defines.h
@@ -0,0 +1,18 @@
+/**
+ * Grupo 8
+ * João Pedro Gomes - 13839069
+ * Luana Hartmann Franco da Cruz - 13676350
+*/
+
+#include "arquivos/defines.h"
+
+#ifndef ARB_DEFINES_INCLUDED
+
+#define ARB_DEFINES_INCLUDED
+
+#define ARB_TAM_PAGINA 205
+#define ARB_TAM_CHAVE 35
+#define ARB_ORDEM 4
+#define ARB_TAM_CHAVE 55
+
+#endif
---
+++ include/arvoreb/tipos.h
@@ -0,0 +1,23 @@
+#include "arvoreb/defines.h"
+
+#ifndef AR_B_TIPOS_INCLUDED
+
+#define AR_B_TIPOS_INCLUDED
+
+typedef struct {
+    char status;
+    int noRaiz;
+    int RRNProxNo;
+} ArBCabecalho;
+
+typedef struct {
+	int nroChavesNo;
+	int alturaNo;
+	int RRNdoNo;
+
+	int RRNFilho[ARB_ORDEM];
+	int RRNArquivoDados[ARB_ORDEM - 1];
+	char chave[ARB_TAM_CHAVE][ARB_ORDEM - 1];
+} ArBNo;
+
+#endif
