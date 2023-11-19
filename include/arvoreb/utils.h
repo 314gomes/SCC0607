2533101c0a21fe6a59a730ba83805c22957bd0c3
João Gomes <joaop.gomes@usp.br>
2023-11-19 14:56:38 -0300

    adiciona auxiliar de abertura

---
+++ include/arvoreb/utils.h
@@ -0,0 +1,1 @@
+FILE *arBAbre(char *caminhoBin);
---
+++ src/arvoreb/utils.c
@@ -0,0 +1,25 @@
+#include "arvoreb/utils.h"
+#include "stdio.h"
+#include "arvoreb/defines.h"
+/// @brief Abrir arquivo de indice binario da Arvore B em modo de leitura
+/// @param caminhobin caminho para o arquivo de indice da Arvore
+/// @return NULL se status for `INCONSISTENTE` ou se arquivo não existir, FILE*
+/// do arquivo binário se tudo correto.
+FILE *arBAbre(char *caminhoBin){
+    char status;
+    
+    FILE *bin = fopen(caminhoBin, "rb");
+    if(bin == NULL){
+        return NULL;
+    }
+
+    fseek(bin, 0, SEEK_SET);
+    fread(&status, 1, 1, bin);
+    
+    if(status == INCONSISTENTE){
+        fclose(bin);
+        return NULL;
+    }
+    
+    return bin;
+}
