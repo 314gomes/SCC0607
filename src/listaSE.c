82dfe313dd9236a22c5a7aefc214482ba7b0f75e
João Gomes <joaop.gomes@usp.br>
2023-12-01 13:37:16 -0300

    Melhorias na legibilidade da lista encadeada

--- src/listaSE.c
+++ src/listaSE.c
@@ -7,6 +7,8 @@
 #include "stdlib.h"
 #include "string.h"
 
+/// @brief Inicializa nova lista
+/// @return lista incializada
 listaSE novaLista(){
 	listaSE n;
 	n.inicio = NULL;
@@ -14,6 +16,9 @@ listaSE novaLista(){
 	return n;
 }
 
+/// @brief Aloca memoria para novo no contendo `str`
+/// @param str conteudo do novo no
+/// @return ponteiro para novo no
 No *novoNo(char *str){
 	No *n = NULL;
 	n = malloc(sizeof(No));
@@ -22,6 +27,12 @@ No *novoNo(char *str){
 	return n;
 }
 
+/// @brief Cria novo no entre os nos `p` (previous) e `n` (next) contendo `str`,
+/// atualizando automaticamente l->incio e l->tam
+/// @param l lista na qual inserir
+/// @param str conteudo do novo no
+/// @param p ponteiro para no anterior
+/// @param n ponteiro para proximo no
 void insereNoPosicao(listaSE *l, char *str, No *p, No *n){
 	No *new = novoNo(str);
 	new->prox = n;
@@ -36,53 +47,63 @@ void insereNoPosicao(listaSE *l, char *str, No *p, No *n){
 	l->tam++;
 }
 
+/// @brief Cria novo no contendo `str` e insere em `l` mantendo ordenacao e
+/// evitando repeticoes.
+/// @param str string do novo no
+/// @param l lista na qual inserir
 void insereOrdenadoSemRepeticao(char *str, listaSE *l){
 	No *atual = l->inicio;
 	No *prev = NULL;
 
+	// percorrer lista
 	while(atual != NULL){
+		// comparar string de insercao com o no atual
 		int cmp = strcmp (str, atual->str);
 
-		// equal strings
+		// caso strings iguais
 		if(cmp == 0){
-			// no insertion
+			// retornar sem insercao
 			return;
 		}
-		// str < l in alphabetical order
+		// str < n em ordem alfabetica
 		else if(cmp > 0){
-			// insert in reverse alphabetical order
-			
+			// insere em ordem alfabetica			
 			insereNoPosicao(l, str, prev, atual);
 			return;
 		}
 
+		// avancar nos nos
 		prev = atual;
 		atual = atual->prox;
 	}
 
-	// Could not find previous matches: place at end
+	// nao achou outra posicao de insercao. inserir no final.
 	insereNoPosicao(l, str, prev, atual);
 	
 }
 
+/// @brief Libera memoria do no `n`
+/// @param n no a ser liberado
 void destroiNo(No *n){
 	free(n);
 }
 
+/// @brief Libera memoria da lista `l` (e de todos seus nos)
+/// @param l lista a ser liberada
 void destroiLista(listaSE *l){
-	No* tmp;
-	// start from l->inicio
+	No* prox_no;
+	// inciar de l->inicio
 	No *atual = l->inicio;
 
 	while(atual != NULL){
-		// save needed content to temporary variable
-		tmp = atual->prox;
+		// salvar proximo no temporariamente
+		prox_no = atual->prox;
 
-		// free node memory
+		// liberar memoria do no
 		destroiNo(atual);
 
-		// jump to next node
-		atual = tmp;
+		// avancar no no
+		atual = prox_no;
 
 	}
 }
